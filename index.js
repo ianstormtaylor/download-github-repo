6eeb2924bb3a5e97bbe28e8efeda74389a643aed
Justin J. Novack <jnovack@gmail.com>
2016-03-23 11:29:34 -0400

    Use ES6 Promises

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 var wget = require('download');
+var request = require('request-promise');
 
 /**
  * Expose `download`.
  */
 
-module.exports = download;
+module.exports = { download: download, query: query };
 
 /**
- * Download GitHub `repo` to `dest` and callback `fn(err)`.
+ * Query GitHub `repo` for commit hash.
+ *
+ * @param {String} repo
+ * @return {Promise} Returns commit hash.
+ */
+
+function query(repo) {
+  return new Promise((resolve, reject) => {
+    var url = api(normalize(repo));
+    request({ uri: url, json: true, headers: { 'User-Agent': 'Request-Promise'} })
+      .then(json => {
+        resolve(json.sha);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
+}
+
+
+/**
+ * Download GitHub `repo` to `dest`
  *
  * @param {String} repo
  * @param {String} dest
- * @param {Function} fn
+ * @return {Promise} Returns commit hash.
  */
 
-function download(repo, dest, fn){
-  var url = github(normalize(repo));
-  var dl = wget(url, dest, { extract: true, strip: 1 });
+function download(repo, dest) {
+  console.log("downloading", repo, dest);
+  return new Promise((resolve, reject) => {
+    query(repo).then(commit => {
 
-  dl.on('error', function(err){
-    fn(err);
-  });
+      repo = normalize(repo);
+      repo.commit = commit;
+      var url = github(repo);
+      var dl = wget(url, dest, { extract: true, strip: 1 });
 
-  dl.on('close', function(){
-    fn();
+      dl.on('error', () => { reject; } );
+      dl.on('close', () => { resolve(commit); } );
+
+    }).catch(reject);
   });
 }
 
@@ -40,8 +66,24 @@ function github(repo){
     + '/'
     + repo.name
     + '/archive/'
-    + repo.branch
+    + (repo.commit || "master")
     + '.zip';
+}
+
+/**
+ * Return a GitHub API url for a given `repo` object.
+ *
+ * @param {Object} repo
+ * @return {String}
+ */
+
+function api(repo){
+  return 'https://api.github.com/repos/'
+    + repo.owner
+    + '/'
+    + repo.name
+    + '/commits/'
+    + repo.branch;
 }
 
 /**
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 
 var assert = require('assert');
-var download = require('..');
+var repo = require('..');
 var read = require('fs-readdir-recursive');
 var rm = require('rimraf').sync;
 
@@ -10,23 +10,50 @@ describe('download-github-repo', function(){
   });
 
   it('downloads the master branch by default', function(done){
-    download('zeke/download-github-repo-fixture', 'test/tmp', function(err){
-      if (err) return done(err);
+    repo.download('zeke/download-github-repo-fixture', 'test/tmp').then((hash) => {
       var actual = read('test/tmp');
       var expected = read('test/fixtures/master');
       assert.deepEqual(actual, expected);
+      assert.equal(hash, 'ff95faeda055a8061fe45742a46f80b39c633ddd');
       done();
+    })
+    .catch(err => {
+      return done(err);
     });
   });
 
-  it('download branches too', function(done){
-    download('zeke/download-github-repo-fixture#my-branch', 'test/tmp', function(err){
-      if (err) return done(err);
+  it('download custom branch', function(done){
+    repo.download('zeke/download-github-repo-fixture#my-branch', 'test/tmp').then((hash) => {
       var actual = read('test/tmp');
       var expected = read('test/fixtures/my-branch');
       assert.deepEqual(actual, expected);
+      assert.equal(hash, '619f72b64bf3bc9457e4b578373eb675481d2ff5');
       done();
+    })
+    .catch(err => {
+      return done(err);
     });
   });
+
+  it('queries master branch commit', function(done){
+    repo.query('zeke/download-github-repo-fixture').then((hash) => {
+      assert.equal(hash, 'ff95faeda055a8061fe45742a46f80b39c633ddd');
+      done();
+    })
+    .catch(err => {
+      return done(err);
+    });
+  });
+
+  it('queries custom branch commit', function(done){
+    repo.query('zeke/download-github-repo-fixture#my-branch').then((hash) => {
+      assert.equal(hash, '619f72b64bf3bc9457e4b578373eb675481d2ff5');
+      done();
+    })
+    .catch(err => {
+      return done(err);
+    });
+  });
+
 
 });
